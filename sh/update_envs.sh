419f6f66a2ebbb43cd5488fdbba8481362cb7364
Asier Tena Zuazolacigorraga <asier.tena.zu@gmail.com>
2024-11-30 19:23:32 +0100

    WIP: use unsecure connection by now

--- sh/update_envs.sh
+++ sh/update_envs.sh
@@ -5,3 +5,7 @@ source .env
 # Bring the .env files to remote
 scp -i ${KEY_PATH} .env ${USER}@${HOST}:${CLONED_IN}/
 scp -i ${KEY_PATH} ./app/.env ${USER}@${HOST}:${CLONED_IN}/app/
+scp -i ${KEY_PATH} ./broker/config/custom_ca.crt ${USER}@${HOST}:${CLONED_IN}/broker/config/
+scp -i ${KEY_PATH} ./broker/config/custom_server.crt ${USER}@${HOST}:${CLONED_IN}/broker/config/
+scp -i ${KEY_PATH} ./broker/config/custom_server.key ${USER}@${HOST}:${CLONED_IN}/broker/config/
+scp -i ${KEY_PATH} ./broker/config/custom.pwfile ${USER}@${HOST}:${CLONED_IN}/broker/config/
