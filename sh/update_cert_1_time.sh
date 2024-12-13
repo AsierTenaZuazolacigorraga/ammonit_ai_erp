d7783f13e99ae75392ddfa0514d00f661884f627
Asier Tena Zuazolacigorraga <asier.tena.zu@gmail.com>
2024-12-13 18:41:00 +0100

    WIP: refactor some env

--- sh/update_cert_1_time.sh
+++ sh/update_cert_1_time.sh
@@ -3,9 +3,9 @@
 source .env
 
 ssh -i "$KEY_PATH" -t "$USER@$HOST" << EOF
-    sudo certbot delete --noninteractive --cert-name ${DOMAIN}
-    sudo certbot delete --noninteractive --cert-name www.${DOMAIN}
-    sudo certbot certonly --standalone --agree-tos --domains ${DOMAIN} --domains www.${DOMAIN} --email ${EMAIL}
-    sudo cp /etc/letsencrypt/live/${DOMAIN}/fullchain.pem /home/ubuntu/projects/${REPO}/certs
-    sudo cp /etc/letsencrypt/live/${DOMAIN}/privkey.pem /home/ubuntu/projects/${REPO}/certs
+    sudo certbot delete --noninteractive --cert-name ${WEB_DOMAIN}
+    sudo certbot delete --noninteractive --cert-name www.${WEB_DOMAIN}
+    sudo certbot certonly --standalone --agree-tos --domains ${WEB_DOMAIN} --domains www.${WEB_DOMAIN} --email ${EMAIL}
+    sudo cp /etc/letsencrypt/live/${WEB_DOMAIN}/fullchain.pem /home/ubuntu/projects/${GIT_REPO}/certs
+    sudo cp /etc/letsencrypt/live/${WEB_DOMAIN}/privkey.pem /home/ubuntu/projects/${GIT_REPO}/certs
 EOF
