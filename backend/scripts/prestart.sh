d9761358c3f5d18ff610e1c3c5f6d70863d8e7e1
Asier Tena Zuazolacigorraga <asier.tena.zu@gmail.com>
2025-05-15 16:55:59 +0200

    WIP: fix last issues with paths

--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -4,10 +4,10 @@ set -e
 set -x
 
 # Let the DB start
-python prestart/backend_pre_start.py
+python app/prestart/backend_pre_start.py
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python prestart/initial_data.py
+python app/prestart/initial_data.py
